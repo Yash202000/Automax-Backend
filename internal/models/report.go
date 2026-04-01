804a856ee01c2a11fe11cd755028cef827b17c5c
iamgak007 <k.gufran@leadergroup.com>
2026-04-01 13:34:57 +0530

    Add user performance report execution and filtering capabilities

--- internal/models/report.go
+++ internal/models/report.go
@@ -103,7 +103,7 @@ type ReportCreateRequestConfig struct {
 type ReportCreateRequest struct {
 	Name        string                    `json:"name" validate:"required,max=255"`
 	Description string                    `json:"description"`
-	DataSource  string                    `json:"data_source" validate:"required,oneof=incidents action_logs users workflows departments locations classifications requests"`
+	DataSource  string                    `json:"data_source" validate:"required,oneof=incidents action_logs users users_performance workflows departments locations classifications requests"`
 	Config      ReportCreateRequestConfig `json:"config" validate:"required"`
 	IsPublic    bool                      `json:"is_public"`
 }
@@ -124,7 +124,7 @@ type ReportExecuteRequest struct {
 
 // ReportExportRequest is used for exporting reports
 type ReportExportRequest struct {
-	DataSource string               `json:"data_source" validate:"required,oneof=incidents action_logs users workflows departments locations classifications request"`
+	DataSource string               `json:"data_source" validate:"required,oneof=incidents action_logs users users_performance workflows departments locations classifications request"`
 	Columns    []ColumnField        `json:"columns" validate:"required,min=1"`
 	Filters    []ReportFilterConfig `json:"filters"`
 	Sorting    []ReportSortConfig   `json:"sorting"`
