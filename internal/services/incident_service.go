756d3e8fec1f7f38e6b96cf6068d7eb35f962f53
iamgak007 <k.gufran@leadergroup.com>
2026-09-17 21:52:23 +0530

    added conditon for vd2 and other clients

--- internal/services/incident_service.go
+++ internal/services/incident_service.go
@@ -714,7 +714,8 @@ func (s *incidentService) CreateIncident(ctx context.Context, req *models.Incide
 		Source:         req.Source,
 	}
 
-	if strings.EqualFold(strings.ToLower(req.Source), strings.ToLower(constants.INCIDENT_SOURCE.WEB)) {
+	if strings.EqualFold(strings.ToLower(req.Source), strings.ToLower(constants.INCIDENT_SOURCE.WEB)) ||
+		!strings.EqualFold(s.cfg.ClientCode, constants.CLIENT_CODE.EPM940) {
 		incident.ReporterName = req.ReporterName
 		incident.ReporterEmail = req.ReporterEmail
 		incident.ReporterPhone = req.ReporterPhone
